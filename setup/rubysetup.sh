e6a840aa711bcab39408343f79e663e135a59a11
Colby Williams <colby@colbycheeze.com>
2019-01-05 16:12:36 +0700

    remove atom

--- setup/rubysetup.sh
+++ setup/rubysetup.sh
@@ -3,21 +3,8 @@ source ~/dotfiles/setup/functions.sh
 
 fancy_echo "Configuring Ruby ..."
 
-find_latest_ruby() {
-  rbenv install -l | grep -v - | tail -1 | sed -e 's/^ *//'
-}
+rvm install ruby-head
 
-ruby_version="$(find_latest_ruby)"
-# shellcheck disable=SC2016
-append_to_zshrc 'eval "$(rbenv init - --no-rehash)"' 1
-eval "$(rbenv init -)"
-
-if ! rbenv versions | grep -Fq "$ruby_version"; then
-  RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/local/opt/openssl rbenv install -s "$ruby_version"
-fi
-
-rbenv global "$ruby_version"
-rbenv shell "$ruby_version"
 gem update --system
 gem_install_or_update 'bundler'
 number_of_cores=$(sysctl -n hw.ncpu)
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Welcome to the ColbyCheeZe laptop script!
+# Welcome to the hungdh0x5e laptop script!
 # Be prepared to turn your laptop (or desktop)
 # into an awesome development machine.
 
 # Helpers
-source ~/dotfiles/setup/applications.sh
+# source ~/dotfiles/setup/applications.sh
 source ~/dotfiles/setup/dotfiles.sh
-source ~/dotfiles/setup/atom.sh
-source ~/dotfiles/setup/shell.sh
-source ~/dotfiles/setup/rubysetup.sh
+# source ~/dotfiles/setup/shell.sh
+# source ~/dotfiles/setup/rubysetup.sh
 source ~/dotfiles/setup/osxsettings.sh
