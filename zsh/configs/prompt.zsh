3742db2af00e72286dd5eaf8601c967678f48145
Huy Hùng <huyhung1994@gmail.com>
2019-01-08 15:55:46 +0700

    zsh: add personal link to gcloud script

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -12,3 +12,9 @@ setopt promptsubst
 if ! env | grep -q '^PS1='; then
   PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
 fi
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/hungdh/workspace/Onaclover/firebase-admin/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/hungdh/workspace/Onaclover/firebase-admin/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/hungdh/workspace/Onaclover/firebase-admin/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/hungdh/workspace/Onaclover/firebase-admin/google-cloud-sdk/completion.zsh.inc'; fi
