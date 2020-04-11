31121519f17c311b38cc7f3db2d525855e12838c
Huy Hùng <huyhung1994@gmail.com>
2020-04-11 23:28:16 +0700

    remove personal path oh-my-zsh

--- setup/applications.sh
+++ setup/applications.sh
@@ -66,10 +66,10 @@ brew cask install dropbox
 brew cask install cloudapp
 brew cask install divvy
 brew cask install skype
-#brew cask install caffeine
-#brew cask install balsamiq-mockups
+# brew cask install caffeine
+# brew cask install balsamiq-mockups
 brew cask install screenflow
-#brew cask install zoomus
+# brew cask install zoomus
 brew cask install postman
 brew cask install macdown
 brew cask install 1password
@@ -78,15 +78,17 @@ fancy_echo "Installing Misc Apps"
 brew cask install slack
 brew cask install vlc
 brew cask install spotify
-#brew cask install steam
-#brew cask install kindle
-
+# brew cask install steam
+# brew cask install kindle
+ 
 fancy_echo "Setting up Node with NVM"
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-#source ~/dotfiles/setup/shell.sh
+
+source ~/dotfiles/setup/shell.sh
+
 nvm install node
 nvm alias default node
 
--- setup/pre-setup.sh
+++ setup/pre-setup.sh
@@ -28,23 +28,3 @@ fi
 fancy_echo "Installing git with Homebrew ..."
 brew install git
 brew tap caskroom/cask
-brew cask install github-desktop
-
-fancy_echo "Setting up Github SSH key pairs."
-echo "Please enter your github email."
-read github_email
-ssh-keygen -t rsa -b 4096 -C $github_email
-
-fancy_echo "Starting ssh-agent in the background."
-eval "$(ssh-agent -s)"
-
-fancy_echo "Adding your SSH key to ssh-agent."
-ssh-add ~/.ssh/id_rsa
-
-fancy_echo "Copying SSH key to your clipboard."
-pbcopy < ~/.ssh/id_rsa.pub
-
-fancy_echo "Add key to github to finish setup."
-echo "Press enter to open instructions."
-read throwaway_input
-open https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/
