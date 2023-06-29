acd0061db3f9530c074e457436041c2e5d27f224
Huy Hùng <huyhung1994@gmail.com>
2023-06-29 14:26:57 +0700

    feat(nvim): custom config for NvChad

---
+++ nvchadrc/autocmds.lua
@@ -0,0 +1,74 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+local function augroup(name)
+	return vim.api.nvim_create_augroup("hungdh_" .. name, { clear = true })
+end
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- auto-save when switching window or leaving vim
+autocmd({ "FocusLost", "WinLeave" }, {
+	pattern = { "*" },
+	group = augroup("auto_save"),
+	desc = "auto-save when switching window or leaving vim",
+	command = ":silent! wa | echo 'saved'",
+})
+
+autocmd("TextYankPost", {
+	desc = "highlight text after is copied",
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 100 })
+	end,
+})
+
+-- wrap and check for spell in text filetypes
+autocmd("FileType", {
+	group = augroup("wrap_spell"),
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt_local.textwidth = 100
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
+-- close some filetypes with <q>
+autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
---
+++ nvchadrc/mappings.lua
@@ -0,0 +1,61 @@
+---@type MappingsTable
+local M = {}
+
+M.disabled = {
+	n = {
+		-- disable for nvim_tmux_navigation
+		["<C-h>"] = "",
+		["<C-j>"] = "",
+		["<C-k>"] = "",
+		["<C-l>"] = "",
+
+		-- no need terminal in nvim
+		["<leader>h"] = "",
+
+		["<Esc>"] = "",
+	},
+}
+
+M.general = {
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<leader>,"] = { ":noh<cr>", "clear highlight after search", opts = { nowait = true } },
+		["<leader><leader>"] = { "<C-^>", "switch the last two files", opts = { nowait = true } },
+
+		["<C-Right>"] = { ":vertical resize +5<cr>", opts = { silent = true } },
+		["<C-Left>"] = { ":vertical resize -5<cr>", opts = { silent = true } },
+		["<C-Up>"] = { ":resize +5<cr>", opts = { silent = true } },
+		["<C-Down>"] = { ":resize -5<cr>", opts = { silent = true } },
+
+		-- Whatever you delete, make it go away
+		["c"] = { '"_c' },
+		["C"] = { '"_C' },
+		["x"] = { '"_x' },
+		["X"] = { '"_c' },
+
+		-- Search will center on the line it's found in
+		["n"] = { "nzzzv" },
+		["N"] = { "Nzzzv" },
+		["#"] = { "#zz" },
+		["*"] = { "*zz" },
+		["g;"] = { "g;zz" },
+	},
+
+	v = {
+		["<"] = { "<gv", "Stay in visual mode during outdent" },
+		[">"] = { ">gv", "Stay in visual mode during indent" },
+	},
+}
+
+-- more keybinds!
+M.nvim_tmux_navigation = {
+	n = {
+		["<C-h>"] = { ":NvimTmuxNavigateLeft<CR>", "Move to window left", opts = { silent = true } },
+		["<C-j>"] = { ":NvimTmuxNavigateDown<CR>", "Move to window down", opts = { silent = true } },
+		["<C-k>"] = { ":NvimTmuxNavigateUp<CR>", "Move to window up", opts = { silent = true } },
+		["<C-l>"] = { ":NvimTmuxNavigateRight<CR>", "Move to window right", opts = { silent = true } },
+		["<C-\\>"] = { ":NvimTmuxNavigateLastActive<CR>", "Move to window last active", opts = { silent = true } },
+	},
+}
+
+return M
