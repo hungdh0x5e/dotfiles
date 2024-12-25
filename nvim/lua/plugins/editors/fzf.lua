2c68abb41c4875c95c05c9256c7d24d9981bb891
Huy Hùng <huyhung1994@gmail.com>
2024-12-25 14:57:46 +0700

    feat(nvim): update fzf keymap

--- nvim/lua/plugins/editors/fzf.lua
+++ nvim/lua/plugins/editors/fzf.lua
@@ -2,46 +2,55 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "[F]ind [F]iles" } },
-    { "<leader>fo", "<cmd>FzfLua oldfiles<CR>", { desc = "[F]ind [O]ld files" } },
-    { "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<CR>", { desc = "[F]ind [B]uffers" } },
-    { "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "[F]ind [G]rep" } },
-    { "<leader>fw", "<cmd>FzfLua grep_cword<CR>", { desc = "[F]ind [W]ord under cursor" } },
+    { "<leader>f?", "<cmd>FzfLua builtin<CR>", desc = "[F]ind providers" },
+
+    -- common
+    { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "[F]ind [F]iles" },
+    { "<leader>fF", "<cmd>FzfLua files resume=true<CR>", desc = "[F]ind [F]iles resume" },
+    { "<leader>fo", "<cmd>FzfLua oldfiles<CR>", desc = "[F]ind [O]ld files" },
+    { "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<CR>", desc = "[F]ind [B]uffers" },
+    { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "[F]ind [G]rep" },
+    { "<leader>fG", "<cmd>FzfLua live_grep_resume<CR>", desc = "[F]ind [G]rep resume" },
+    { "<leader>fc", "<cmd>FzfLua grep_curbuf<CR>", desc = "[F]ind in [C]urrent buffer" },
+    { "<leader>fw", "<cmd>FzfLua grep_cword<CR>", desc = "[F]ind [W]ord under cursor" },
     { "<leader>fw", "<cmd>FzfLua grep_visual<CR>", mode = "v", desc = "[F]ind [W]ord selection" },
+
+    -- misc
+    { "<leader>f:", "<cmd>FzfLua command_history<cr>", desc = "[F]ind command history" },
+    { "<leader>f/", "<cmd>FzfLua search_history<cr>", desc = "[F]ind search history" },
     { "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "[F]ind [H]elp" },
-    { "<leader>fc", "<cmd>FzfLua command_history<cr>", desc = "[F]ind [C]ommand history" },
     { "<leader>fk", "<cmd>FzfLua keymaps<cr>", desc = "[F]ind [K]eymaps" },
     { "<leader>fq", "<cmd>FzfLua quickfix<cr>", desc = "[F]ind [Q]uickfix" },
 
     -- with resume
-    { "<leader>frf", "<cmd>FzfLua files resume=true<CR>", { desc = "[F]ind [R]esume [F]iles" } },
-    { "<leader>frg", "<cmd>FzfLua live_grep_resume<CR>", { desc = "[F]ind [R]esume [G]rep" } },
+    { "<leader><leader>", "<cmd>FzfLua resume<CR>", { desc = "Open last finder" } },
+
+    -- Git
+    { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Git commit" },
+    { "<leader>gC", "<cmd>FzfLua git_bcommits<CR>", desc = "Git commit buffer" },
 
     -- For LSP keymap
-    { "<leader>ca", "<cmd>FzfLua lsp_code_actions<CR>", { desc = "LSP: [C]ode [A]ction" } },
-    { "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>", { desc = "LSP: [F]ind [s]ymbols in current buffer" } },
-    { "<leader>fS", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", { desc = "LSP: [F]ind [S]ymbols" } },
-    { "<leader>fd", "<cmd>FzfLua diagnostics_workspace<CR>", { desc = "LSP: [F]ind [D]iagnostics" } },
+    { "<leader>ca", "<cmd>FzfLua lsp_code_actions<CR>", desc = "LSP: [C]ode [A]ction" },
+    { "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>", desc = "LSP: [F]ind [s]ymbols buffer" },
+    { "<leader>fS", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", desc = "LSP: [F]ind [S]ymbols" },
+    { "<leader>fd", "<cmd>FzfLua diagnostics_document<CR>", desc = "LSP: [F]ind [D]iagnostics buffer" },
+    { "<leader>fD", "<cmd>FzfLua diagnostics_workspace<CR>", desc = "LSP: [F]ind [D]iagnostics" },
     {
       "gd",
       "<cmd>FzfLua lsp_definitions jump_to_single_result=true ignore_current_line=true<CR>",
-      { desc = "LSP: [G]oto[D]efinition" },
+      desc = "LSP: [G]oto [D]efinition",
     },
     {
       "gi",
       "<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<CR>",
-      { desc = "LSP: [G]oto [I]mplementation" },
+      desc = "LSP: [G]oto [I]mplementation",
     },
     {
       "gr",
       "<cmd>FzfLua lsp_references jump_to_single_result=true ignore_current_line=true<CR>",
-      { desc = "LSP: [G]oto [R]eferences" },
+      desc = "LSP: [G]oto [R]eferences",
     },
-    {
-      "gt",
-      "<cmd>FzfLua lsp_typedefs jump_to_single_result=true ignore_current_line=true<CR>",
-      { desc = "LSP: [G]oto [T]ype references" },
-    },
+    { "gt", "<cmd>FzfLua lsp_typedefs ignore_current_line=true<CR>", desc = "LSP: [G]oto [T]ype references" },
   },
   opts = {
     -- { "default-title" },
@@ -56,6 +65,8 @@ return {
       git_icons = false,
       file_icons = false,
       color_icons = false,
+      -- fuzzy search by direction: filename -> path_to_file
+      -- { formatter={"path.filename_first",2} for fuzzy search: path_to_file -> filename. But highlight not working
       formatter = "path.filename_first",
     },
     keymap = {
